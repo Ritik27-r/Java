04b25bd61022ac3adbef233a03d55cffd2a61b75
Ritik27-r <rawatritik70@gmail.com>
2025-07-28 17:33:57 +0530

    third commit

--- k_multidimension.java
+++ k_multidimension.java
@@ -28,5 +28,19 @@ public class k_multidimension {
             }
             System.out.println();
         }
+
+        System.out.println("Multidimensional Array 3 (Random Initialization):");
+        int arr3[][]=new int[3][4];
+        for(int i = 0; i < arr3.length; i++) {
+            for(int j = 0; j < arr3[i].length; j++) {
+                arr3[i][j] = (int)(Math.random()*100); // Example initialization
+            }
+        }
+        for(int i = 0; i < arr3.length; i++) {
+            for(int j = 0; j < arr3[i].length; j++) {
+                System.out.print(arr3[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
